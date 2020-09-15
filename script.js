a2ca3f3de54671da3f0348dc3686fe6449de30a9
1awe23 <legolas19902001@yahoo.gr>
2020-09-15 14:28:05 +0300

    commented

--- script.js
+++ script.js
@@ -9,109 +9,73 @@ document.querySelector( 'form' ).addEventListener( 'submit', function ( e ) {
 
     var cik = input.value
 
+    // Hide placeholder, start loading, hide previous data
+    
     document.querySelector(".data-placeholder").style.display = "none"
     document.querySelector(".data").style.display = "none"
     document.querySelector( ".loading" ).style.display = "block"
 
     fetchData( cik )
 
-    // mockData()
-
 } )
 
-function mockData() {
-
-    document.querySelector( ".loading" ).style.display = "none"
-    document.querySelector( ".data" ).style.display = "flex"
-
-    var res = {
-        result: {
-            '2020': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2019': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2018': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2017': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2016': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2015': {
-                'roic': '0.8',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-            '2014': {
-                'roic': '0.996545456',
-                'eps': '0.1',
-                'equity': '0.1',
-                'sales': '0.1',
-                'freeCashFlow': '0.1',
-            },
-        }
-    }
-
-    var data = res.result
-
-    var allChartsOptions = {}
 
 
-    
-    
-    
-}
+
+
+
+// Main function
 
 function fetchData ( cik ) {
 
-    var res = fetch( "https://past-company-rates-server.herokuapp.com/rates", { method: "POST", headers: { 'content-type': 'application/json' }, body: JSON.stringify( { cik: cik, year: "2020" } ) } )
+
+    // Fetch the data from API up to current year
+
+    fetch( "https://past-company-rates-server.herokuapp.com/rates", 
+        { 
+            method: "POST", 
+            headers: { 'content-type': 'application/json' }, 
+            body: JSON.stringify( { cik: cik, year: new Date().getFullYear() } ) 
+        } )
         .then( res => res.json() )
         .then( res => {
+
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+            //
+            //      AFTER DATA IS RECEIVED
+            //
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+
+            // Hide loading spinner, show data
 
             document.querySelector(".loading").style.display = "none"
             document.querySelector(".data").style.display = "flex"
 
             var data = res.result
-
-
-
-
-            var years = []
             var allChartsOptions = {}
 
 
+
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+            //
+            //      POPULATE CHART OPTIONS FROM DATA
+            //
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+
+            // For each year
             Object.entries( data )
+                // Sort by year
                 .sort( ( year1, year2 ) => parseInt( year1 ) < parseInt( year2 ) )
                 .forEach( ( [ year, rates ] ) => {
 
+                    // For each financial key
                     Object.entries( rates ).forEach( ( [ key, value ] ) => {
 
+                        // Initialize options for financial key if not exists
                         allChartsOptions[ key ] = allChartsOptions[ key ]
                             || {
                             name: key,
@@ -119,9 +83,11 @@ function fetchData ( cik ) {
                             data: [],
                             averages: []
                         }
-
+                        
+                        // Label for this key is the key (string) itself
                         allChartsOptions[ key ].labels.push( year )
-
+                        
+                        // Append the value for this year and financial key
                         allChartsOptions[ key ].data.push( value )
 
                     } )
@@ -129,7 +95,15 @@ function fetchData ( cik ) {
 
                 } )
 
-            // Calculate averages
+
+
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+            //
+            //      CALCULATE AVERAGES
+            //
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
 
             Object.values( allChartsOptions ).forEach( optionsForKey => {
 
@@ -143,7 +117,6 @@ function fetchData ( cik ) {
                     var pct = average * 100
                     return pct.toFixed( 1 ) + '%'
                 }
-                // var arrAvg = arr => arr.reduce( ( a, b ) => Number(a) + Number(b), 0 ) / arr.length
 
                 var averages = [ getAveragePct( y10 ), getAveragePct( y5 ), getAveragePct( y3 ) ]
 
@@ -152,18 +125,29 @@ function fetchData ( cik ) {
             } );
 
 
-            Chart.defaults.global.defaultFontColor = '#fff';
-            Chart.defaults.global.defaultFontFamily = 'Roboto';
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+            //
+            //      SETUP AND DISPLAY CHARTS PER FINANCIAL KEY
+            //
+            //////////////////////////////////////////////////////////
+            //////////////////////////////////////////////////////////
+
+            // Setup global chart options
+            Chart.defaults.global.defaultFontColor = '#fff'
+            Chart.defaults.global.defaultFontFamily = 'Roboto'
             Chart.Tooltip.positioners.cursor = function ( chartElements, coordinates ) {
-                return coordinates;
-            };
+                return coordinates
+            }
 
+            Object.values( allChartsOptions ).forEach( ( options, index ) => {
 
-            Object.entries( allChartsOptions ).forEach( ( [ key, options ], index ) => {
+                // ctx is the element (canvas)
+                var ctx = document.getElementById( 'chart' + ( index + 1 ) )
 
-                var ctx = document.getElementById( 'chart' + ( index + 1 ) )//.getContext( '2d' );
+                // Chart options
+                var chartOpts = {
 
-                var chart = new Chart( ctx, {
                     type: 'bar',
                     data: {
                         labels: options.labels,
@@ -191,7 +175,7 @@ function fetchData ( cik ) {
                                 gridLines: {
                                     lineWidth: 0,
                                     zeroLineWidth: 1
-
+    
                                 }
                             } ]
                         },
@@ -206,7 +190,7 @@ function fetchData ( cik ) {
                             display: false,
                         },
                         annotation: {
-
+                            // Horizontal line with 15%
                             annotations: [
                                 {
                                     type: 'line',
@@ -220,9 +204,7 @@ function fetchData ( cik ) {
                                         content: '15%'
                                     }
                                 }
-
                             ]
-
                         },
                         tooltips: {
                             mode: 'index',
@@ -234,103 +216,25 @@ function fetchData ( cik ) {
                             yPadding: 12,
                             displayColors: false,
                             callbacks: {
+                                // Display tooltip over mouse
                                 label: function ( tooltipItems, data ) {
                                     return ( Number( tooltipItems.value ) * 100 ).toFixed( 1 ) + '%'
                                 }
                             }
-
+    
                         }
                     },
+                }
 
-                } )
+
+                // Create the chart
+                new Chart( ctx, chartOpts )
 
                 // Display averages
-                ctx.parentElement.querySelector( '.avg-container' ).innerHTML = `<span>Averages</span> 10Y <b>${ options.averages[ 0 ] }</b> / 5Y <b>${ options.averages[ 1 ] }</b> / 3Y <b>${ options.averages[ 2 ] }</b>`
-
+                ctx.parentElement.querySelector( '.avg-container' )
+                    .innerHTML = `<span>Averages</span> 10Y <b>${ options.averages[ 0 ] }</b> / 5Y <b>${ options.averages[ 1 ] }</b> / 3Y <b>${ options.averages[ 2 ] }</b>`
+           
             } )
-
-        //     Object.entries( data )
-        //         .sort( ( year1, year2 ) => parseInt( year1 ) < parseInt( year2 ) )
-        //         .forEach( ( [ year, rates ] ) => {
-
-        //             Object.entries( rates ).forEach( ( [ key, value ] ) => {
-        //                 console.log( key )
-        //                 allChartsOptions[ key ] = allChartsOptions[ key ]
-        //                     || {
-        //                     name: key,
-        //                     labels: [],
-        //                     data: []
-        //                 }
-
-        //                 allChartsOptions[ key ].labels.push( year )
-
-        //                 allChartsOptions[ key ].data.push( value )
-
-        //             } )
-
-
-        //         } )
-
-        //     Object.entries( allChartsOptions ).forEach( ( [ key, options ], index ) => {
-
-        //         var ctx = document.getElementById( 'chart' + ( index + 1 ) ).getContext( '2d' );
-
-                
-        //         var chart = new Chart( ctx, {
-        //             type: 'bar',
-        //             data: {
-        //                 labels: options.labels,
-        //                 datasets: [ {
-        //                     label: options.name,
-        //                     data: options.data,
-        //                     borderWidth: 1
-        //                 } ]
-        //             },
-        //             options: {
-        //                 scales: {
-        //                     yAxes: [ {
-        //                         ticks: {
-        //                             beginAtZero: true,
-        //                             min: -0.2,
-        //                             max: 0.5,
-        //                             callback: function ( v ) {
-        //                                 // Format as percentage
-        //                                 return ( v * 100 ) + '%'
-        //                             }
-        //                         },
-        //                         gridLines: {
-        //                             lineWidth: 0,
-        //                             zeroLineWidth: 1
-                                
-        //                         }
-        //                     } ]
-        //                 },
-        //                 annotation: {
-
-        //                     annotations: [
-        //                         {
-        //                             type: 'line',
-        //                             mode: 'horizontal',
-        //                             scaleID: 'y-axis-0',
-        //                             value: 0.15,
-        //                             borderColor: 'rgb(75, 192, 192)',
-        //                             borderWidth: 1,
-        //                             label: {
-        //                                 enabled: true,
-        //                                 content: '15%'
-        //                             }
-        //                         }
-
-        //                     ]
-
-        //                 }
-        //             },
-
-        //         } )
-
-
-
-        //     } )
 
 
 
@@ -338,7 +242,62 @@ function fetchData ( cik ) {
         } )
 
 
-
 }
 
 
+
+
+
+// var mockRes = {
+//     result: {
+//         '2020': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2019': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2018': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2017': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2016': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2015': {
+//             'roic': '0.8',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//         '2014': {
+//             'roic': '0.996545456',
+//             'eps': '0.1',
+//             'equity': '0.1',
+//             'sales': '0.1',
+//             'freeCashFlow': '0.1',
+//         },
+//     }
+// }
